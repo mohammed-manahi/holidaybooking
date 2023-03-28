375d3bc093c4a71bfbd55626e2986a5b74d90296
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2023-03-28 04:02:49 +0300

    Create account app

--- holidaybooking/settings.py
+++ holidaybooking/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Add created account to installed apps
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
