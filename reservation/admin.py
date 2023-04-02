883587da4af190d32270369793ba3a92013fd9c6
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2023-04-02 03:15:47 +0300

    Register reservation models to admin site

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from reservation.models import Property
+from reservation.models import Property, Media, Feature, FeatureCategory, Review, Category
+
+
+@admin.register(Media)
+class PropertyMedia(admin.ModelAdmin):
+    """
+    Register property media model in admin site
+    """
+    list_display = ['name', 'property']
+    list_filter = ['property']
 
 
 @admin.register(Property)
@@ -8,4 +17,40 @@ class PropertyAdmin(admin.ModelAdmin):
     Register property model in admin site
     """
     list_display = ['name', 'description', 'owner', 'address', 'available']
-    list_filter = ['available']
+    list_filter = ['category', 'available']
+
+
+@admin.register(FeatureCategory)
+class PropertyFeatureCategoryAdmin(admin.ModelAdmin):
+    """
+    Add property media model in admin site
+    """
+    list_display = ['name', 'description']
+    list_filter = ['name']
+
+
+@admin.register(Feature)
+class PropertyFeatureAdmin(admin.ModelAdmin):
+    """
+    Add property media model in admin site
+    """
+    list_display = ['name', 'description']
+    list_filter = ['name']
+
+
+@admin.register(Review)
+class PropertyReviewAdmin(admin.ModelAdmin):
+    """
+    Add property review model in admin site
+    """
+    list_display = ['user', 'property', 'rate']
+    list_filter = ['rate']
+
+
+@admin.register(Category)
+class PropertyCategoryAdmin(admin.ModelAdmin):
+    """
+    Add property review model in admin site
+    """
+    list_display = ['name', 'description', 'slug']
+    list_filter = ['name']
